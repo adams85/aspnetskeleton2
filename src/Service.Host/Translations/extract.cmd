ed73e1a81b0df5e61a994bdbaae86a79845856f1
Adam Simon <adamosimoni@gmail.com>
2020-07-16 14:21:32 +0200

    batch script for extracting localizable texts from projects

---
+++ src/Service.Host/Translations/extract.cmd
@@ -0,0 +1,18 @@
+@ECHO OFF
+
+SETLOCAL EnableDelayedExpansion
+
+SET ApplicationPrefix=WebApp.
+SET Languages=en-US
+SET Projects=Common;Service.Contract;Service;Api;UI.Mvc
+
+FOR %%L IN (%Languages%) DO (
+  SET LanguageSwitches=!LanguageSwitches! -l %%L
+)
+
+FOR %%P IN (%Projects%) DO (
+  echo Extracting %%P...
+  
+  cd "%~dp0\..\..\%%P"
+  dotnet po scan -p %%P.csproj | dotnet po extract -o "%~dp0\%ApplicationPrefix%%%P.pot" -m %LanguageSwitches%
+)

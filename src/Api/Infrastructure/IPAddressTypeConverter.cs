388f6e9f1f81f1639c1b252fa750b6a5ac067462
Adam Simon <adamosimoni@gmail.com>
2022-04-23 22:13:47 +0200

    code formatting

--- src/Api/Infrastructure/IPAddressTypeConverter.cs
+++ src/Api/Infrastructure/IPAddressTypeConverter.cs
@@ -30,6 +30,7 @@ public sealed class IPAddressTypeConverter : TypeConverter
 
         return base.CanConvertTo(context, destinationType);
     }
+
     public override object? ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
     {
         if (destinationType == typeof(string) && value is IPAddress)

1e95ce176c767a8d642b70543032b75621fbfe1a
Adam Simon <adamosimoni@gmail.com>
2026-08-19 01:28:47 +0200

    Fix validation in EnqueueMailCommandHandler

--- src/Service/Mailing/EnqueueMailCommandHandler.cs
+++ src/Service/Mailing/EnqueueMailCommandHandler.cs
@@ -33,7 +33,7 @@ internal sealed class EnqueueMailCommandHandler : CommandHandler<EnqueueMailComm
             await mailTypeDefinition!.ValidateModelAsync(validationContext, cancellationToken).ConfigureAwait(false);
 
             if (validationContext.ErrorCode != null)
-                new ServiceErrorException(validationContext.ErrorCode.Value, validationContext.ErrorArgsFactory?.Invoke());
+                throw new ServiceErrorException(validationContext.ErrorCode.Value, validationContext.ErrorArgsFactory?.Invoke());
 
             await _mailSenderService.EnqueueItemAsync(command.Model, dbContext, null, cancellationToken).ConfigureAwait(false);
         }
